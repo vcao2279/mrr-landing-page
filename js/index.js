bdd1a02dd05aefb9774f8796d9bde14ddc840b81
Vu Cao <cdhvu@yahoo.com>
2019-02-22 15:13:13 -0800

    Creating an automatic slider

--- js/index.js
+++ js/index.js
@@ -11,3 +11,14 @@ hamburger.addEventListener("click", () => {
     ? hamburger.classList.replace("fa-bars", "fa-times")
     : hamburger.classList.replace("fa-times", "fa-bars");
 });
+
+const slides = document.querySelectorAll(".slider-images");
+let currentSlide = 0;
+slides[currentSlide].classList.add("showing");
+let slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("showing");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("showing");
+}
